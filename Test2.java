41f786730de0896df73559f9f2d48d4bb9a26f01
Enes Ugras <eugras71@gmail.com>
2025-06-06 20:50:06 +0300

    my third commit

---
+++ Test2.java
@@ -0,0 +1,8 @@
+public class Test2 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello from commit 3");
+        System.out.println("This is a change in commit 3");
+        System.out.println("This is another change in commit 3");
+    }
+}
