17497d80608adc4df293a3ec052d5837af4fe74c
Eugrass <147923229+Eugrass@users.noreply.github.com>
2025-06-06 22:25:44 +0300

    Update Test2.java
    Add new features

--- Test2.java
+++ Test2.java
@@ -4,5 +4,7 @@ public class Test2 {
         System.out.println("Hello from commit 3");
         System.out.println("This is a change in commit 3");
         System.out.println("This is another change in commit 3");
+
+        System.out.println("NEW FEATURES");
     }
 }
