93514faabc45993e7c82c7fa2ca44fa60419c639
Enes Ugras <eugras71@gmail.com>
2025-06-06 23:03:30 +0300

    add login feature

--- Login.java
+++ Login.java
@@ -3,6 +3,6 @@ public class Login {
     public static void main(String[] args) {
 
         System.out.println("Login successful");
-        
+        System.out.println("Welcome to the application!");
     }
 }
