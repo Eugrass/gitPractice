8138a916a773a051293b880feaa232789260c950
Enes Ugras <eugras71@gmail.com>
2025-06-06 20:30:47 +0300

    my second commit

--- Test1.java
+++ Test1.java
@@ -2,5 +2,8 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("Hello from commit 1");
+
+        System.out.println("Hello from commit 2");
+        System.out.println("This is a change in commit 2");
     }
 }
